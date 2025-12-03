dc87d21efdd9f9a6c4211add5486b36f80ec5623
CallMeDang123 <NhatN545@gmail.com>
2025-12-03 17:00:08 +0700

    Bổ xung thêm URL img, fix update img, thêm quản lý feedback

--- admin/product/editor.php
+++ admin/product/editor.php
@@ -8,20 +8,20 @@ require_once('form_save.php');
 
 $id = getGet('id');
 if ($id != '' && $id > 0) {
-  $sql = "select * from Product where id = '$id' and deleted = 0";
-  $userItem = executeResult($sql, true);
-  if ($userItem != null) {
-    $thumbnail = $userItem['thumbnail'];
-    $title = $userItem['title'];
-    $price = $userItem['price'];
-    $discount = $userItem['discount'];
-    $category_id = $userItem['category_id'];
-    $description = $userItem['description'];
-  } else {
-    $id = 0;
-  }
+    $sql = "select * from Product where id = '$id' and deleted = 0";
+    $userItem = executeResult($sql, true);
+    if ($userItem != null) {
+        $thumbnail = $userItem['thumbnail'];
+        $title = $userItem['title'];
+        $price = $userItem['price'];
+        $discount = $userItem['discount'];
+        $category_id = $userItem['category_id'];
+        $description = $userItem['description'];
+    } else {
+        $id = 0;
+    }
 } else {
-  $id = 0;
+    $id = 0;
 }
 
 $sql = "select * from Category";
@@ -37,53 +37,60 @@ $categoryItems = executeResult($sql);
         <div class="panel panel-primary">
             <div class="panel-body">
                 <form method="post" style="font-weight: 700" enctype="multipart/form-data">
-                <div class="row">
-                    <div class="col-md-9 col-12">
-                        <div class="form-group">
-                            <label for="usr">Tên Sản Phẩm:</label>
-                            <input required="true" type="text" class="form-control" id="usr" name="title" value="<?= $title ?>">
-                            <input type="text" name="id" value="<?= $id ?>" hidden="true">
+                    <div class="row">
+                        <div class="col-md-9 col-12">
+                            <div class="form-group">
+                                <label for="usr">Tên Sản Phẩm:</label>
+                                <input required="true" type="text" class="form-control" id="usr" name="title" value="<?= $title ?>">
+                                <input type="text" name="id" value="<?= $id ?>" hidden="true">
+                            </div>
+                            <div class="form-group">
+                                <label for="description">Mô tả:</label>
+                                <textarea class="form-control" rows="5" name="description" id="description"><?= $description ?></textarea>
+                            </div>
+                            <button class="btn btn-success" style="background-color: rgba(0, 0, 255, 0.621);  width: 20%; font-weight: bold">Lưu</button>
                         </div>
-                        <div class="form-group">
-                            <label for="description">Mô tả:</label>
-                            <textarea class="form-control" rows="5" name="description" id="description"><?=$description?></textarea>
+                        <div class="col-md-3 col-12">
+
+                            <div class="form-group" style="margin-bottom: 8px">
+                                <!-- <label for="thumbnail">Thumbnail:</label>
+                                <input required="true" type="text" class="form-control" id="thumbnail" name="thumbnail" value="<?= $thumbnail ?>" onchange="updateThumbnail()">
+                                <img id="thumbnail_img" src="<?= $thumbnail ?>" style="max-height: 200px; margin-top: 5px; margin-bottom: 10px; max-width: 100%;"> -->
+                                <label for="thumbnail thumbnail_url">Thumbnail (File/URL):</label>
+                                <input type="file" class="form-control" id="thumbnail" name="thumbnail" accept=".avif, .jpg, .png, .jpeg, .gif, .bmp, .tif, .tiff|image/*" onchange="updateThumbnail()" style="margin-bottom: 10px;">
+                                <!-- <img id=" thumbnail_img" src="<?= fixUrl($thumbnail) ?>" style="max-height: 200px; margin-top: 5px; margin-bottom: 10px; max-width: 100%;"> -->
+                                <input type="text" class="form-control" id="thumbnail_url" name="thumbnail_url" value="<?= (strpos($thumbnail, 'http') !== false ? $thumbnail : '') ?>" placeholder="Nhập URL..." oninput="updateThumbnail()">
+                            </div>
+
+                            <div class="form-group">
+                                <img id="thumbnail_img" src="<?= ($thumbnail != '') ? fixUrl($thumbnail) : 'https://placehold.co/600x400?text=No+Image' ?>" style="max-height: 160px; margin-top: 5px; margin-bottom: 10px; max-width: 100%; object-fit: contain; border: 1px solid #ccc;">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="category_id">Danh Mục Sản Phẩm:</label>
+                                <select class="form-control" name="category_id" id="category_id" required="true">
+                                    <option value="">-- Chọn --</option>
+                                    <?php
+                                    foreach ($categoryItems as $item) {
+                                        if ($item['id'] == $category_id) {
+                                            echo '<option selected value="' . $item['id'] . '">' . $item['name'] . '</option>';
+                                        } else {
+                                            echo '<option value="' . $item['id'] . '">' . $item['name'] . '</option>';
+                                        }
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="price">Giá:</label>
+                                <input required="true" type="number" class="form-control" id="price" name="price" value="<?= $price ?>">
+                            </div>
+                            <div class="form-group">
+                                <label for="discount">Giảm Giá:</label>
+                                <input required="true" type="number" class="form-control" id="discount" name="discount" value="<?= $discount ?>">
+                            </div>
                         </div>
-                        <button class="btn btn-success" style="background-color: rgba(0, 0, 255, 0.621);  width: 20%; font-weight: bold">Lưu</button>
                     </div>
-                    <div class="col-md-3 col-12">
-                        <div class="form-group">
-                            <!-- <label for="thumbnail">Thumbnail:</label>
-                            <input required="true" type="text" class="form-control" id="thumbnail" name="thumbnail" value="<?= $thumbnail ?>" onchange="updateThumbnail()">
-                            <img id="thumbnail_img" src="<?= $thumbnail ?>" style="max-height: 200px; margin-top: 5px; margin-bottom: 10px; max-width: 100%;"> -->
-                            <label for="thumbnail">Thumbnail:</label>
-                            <input required="true" type="file" class="form-control" id="thumbnail" name="thumbnail" accept=".avif, .jpg, .png, .jpeg, .gif, .bmp, .tif, .tiff|image/*">
-                            <img id="thumbnail_img" src="<?= fixUrl($thumbnail) ?>" style="max-height: 200px; margin-top: 5px; margin-bottom: 10px; max-width: 100%;">
-                        </div>
-                        <div class="form-group">
-                            <label for="category_id">Danh Mục Sản Phẩm:</label>
-                            <select class="form-control" name="category_id" id="category_id" required="true">
-                            <option value="">-- Chọn --</option>
-                            <?php
-                            foreach ($categoryItems as $item) {
-                                if ($item['id'] == $category_id) {
-                                echo '<option selected value="' . $item['id'] . '">' . $item['name'] . '</option>';
-                                } else {
-                                echo '<option value="' . $item['id'] . '">' . $item['name'] . '</option>';
-                                }
-                            }
-                            ?>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label for="price">Giá:</label>
-                            <input required="true" type="number" class="form-control" id="price" name="price" value="<?= $price ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="discount">Giảm Giá:</label>
-                            <input required="true" type="number" class="form-control" id="discount" name="discount" value="<?= $discount ?>">
-                        </div>
-                    </div>
-                </div>
                 </form>
             </div>
         </div>
@@ -105,6 +112,34 @@ $categoryItems = executeResult($sql);
             ['view', ['fullscreen', 'codeview', 'help']]
         ]
     });
+</script>
+
+<script>
+    function updateThumbnail() {
+        var fileInput = document.getElementById('thumbnail');
+        var urlInput = document.getElementById('thumbnail_url');
+        var img = document.getElementById('thumbnail_img');
+
+        // Ưu tiên: Nếu có chọn file thì đọc file
+        if (fileInput.files && fileInput.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                img.src = e.target.result; // Cập nhật ảnh preview bằng dữ liệu file
+            }
+            reader.readAsDataURL(fileInput.files[0]);
+        }
+        // Nếu không chọn file mà có nhập URL
+        else if (urlInput.value && urlInput.value.trim() !== '') {
+            img.src = urlInput.value; // Cập nhật ảnh preview bằng URL
+        }
+        // Nếu không có gì cả
+        else {
+            // Giữ nguyên ảnh cũ hoặc về ảnh mặc định (tuỳ nhu cầu), ở đây ta để về placeholder
+            // Tuy nhiên logic tốt nhất là nếu field trống thì giữ nguyên hiển thị cũ (do PHP load), 
+            // nhưng để đơn giản ta hiển thị placeholder để báo hiệu đang trống.
+            img.src = "https://placehold.co/600x400?text=No+Image";
+        }
+    }
 </script>
 <!-- <script type="text/javascript">
     function updateThumbnail() {
--- admin/product/form_save.php
+++ admin/product/form_save.php
@@ -1,26 +1,31 @@
 <?php
-if(!empty($_POST)) {
+if (!empty($_POST)) {
     $id = getPost('id');
     $title = getPost('title');
     $price = getPost('price');
     $discount = getPost('discount');
-    
+
+    // 1. Thử lấy file upload
     $thumbnail = moveFile('thumbnail');
-    
+
+    // 2. Nếu không có file upload, thử lấy từ ô nhập URL
+    if (empty($thumbnail)) {
+        $thumbnail = getPost('thumbnail_url');
+    }
+
     $description = getPost('description');
     $category_id = getPost('category_id');
     $created_at = $updated_at = date('Y-m-d H:s:i');
-    
-    if($id > 0) {
+
+    if ($id > 0) {
         //update
-        if($thumbnail != null) {
+        if ($thumbnail != '') {
             $sql = "update Product set thumbnail = '$thumbnail', title = '$title', price = '$price', discount = '$discount', description = '$description', updated_at = '$updated_at', category_id = '$category_id' where id = $id";
         } else {
             $sql = "update Product set title = '$title', price = '$price', discount = '$discount', description = '$description', updated_at = '$updated_at', category_id = '$category_id' where id = $id";
         }
-        
-        execute($sql);
 
+        execute($sql);
         header('Location: index.php');
         die();
     } else {
